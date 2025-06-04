9e326cfdcab509da209c73006d945a84f7e81a83
consysadn <52031038+consysadn@users.noreply.github.com>
2025-06-04 15:00:29 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import shared_library
+import json
+import Agents
+import OpenTickets
+import ClosedTickets
+import Organizations
+import People
+import scp
+import cifs
+
+# SMB to veeam.consys.it
+remote_hostname = "veeam.consys.it"
+remote_hostip = "192.168.11.16"
+remote_share = "Consys"
+user = "user"
+pwd = "Parampa1994!"
+remote_path = 'DeskPro_Backup'
+local_path = '/var/deskpro/Backup_files'
+
+
+Agents.get_data()
+Organizations.get_data()
+People.get_data()
+OpenTickets.get_data()
+ClosedTickets.get_data()
+#scp.transfert_data(local_path, remote_path, remote_host, user, pwd)
+cifs.transfert_data(local_path, remote_path, remote_hostname, remote_hostip, remote_share, user, pwd)
